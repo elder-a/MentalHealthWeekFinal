621017f13c20159f60574eb4cfb100edbdeb0ff4
Jeffrey Goldsmith <jgoldsmith@rsgc.on.ca>
2017-01-26 09:20:12 -0500

    Finished breaking up everything into classes

--- MentalHealthWeek/main.swift
+++ MentalHealthWeek/main.swift
@@ -7,19 +7,17 @@
 //
 
 import Foundation
-
+var emptyArray : [[String]] = [[],[],[],[],[]] // Empty array to be used to initialize array of activities
 
 //
 // Define Globals.
 //
-let gradeChoiceLookup : [Int : Int] = [9 : 0, 10 : 27, 11 : 55, 12 : 83]
-let gradeChoiceNum = [27, 28, 28, 29] // 27, 28, 28, 29
-let activityChoiceOffset = 12
-var columnDescriptors : [String] = []
-var descriptorLookup : [String] = []
-var emptyArray : [[String]] = [[],[],[],[],[]]
-var students : [Student] = []
-var activities : [Activity] = [
+let gradeChoiceNum = [27, 28, 28, 29] // Number of survey questions per grade
+let activityChoiceOffset = 12 // Number of survey questions before the activity choices
+var columnDescriptors : [String] = [] // An array for the column information
+var titles : [String] = [] // An array to hold the titles
+var students : [Student] = [] // An array of all the students
+var activities : [Activity] = [ // An array of all the activities
     Activity(weekdays: emptyArray, shortName: "MathExam", name: "Math Exam", personCap: 500, supervisorName: "Mr. Fitz"),
     Activity(weekdays: emptyArray, shortName: "Sleep", name: "Sleep In", personCap: 500, supervisorName: "Mr. Fitz"),
     Activity(weekdays: emptyArray, shortName: "Breakfast", name: "Casual Breakfast", personCap: 160, supervisorName: "Ms. Totten"),
@@ -30,151 +28,32 @@ var activities : [Activity] = [
     Activity(weekdays: emptyArray, shortName: "Animals", name: "Animal Therapy", personCap: 16, supervisorName: "Ms. Kaye/Fitz"),
     Activity(weekdays: emptyArray, shortName: "Massage", name: "Massage", personCap: 12, supervisorName: "Ms."),
 ]
-var weekdayLookup : [String : Int] = ["Monday" : 0, "Tuesday" : 1, "Wednesday" : 2, "Thursday" : 3, "Friday" : 4]
+var input = Input() // Initialize input class
 
-
-// Read the text file (place in your home folder)
-// Path will probably be /Users/student/survey_response_sample.txt
-// Obtain the data file on Haiku, Day 37
-guard let reader = LineReader(path: "/Users/student/Desktop/MentalHealthWeekFinal/survey_response_all_data_new_headers.csv") else {
+// Open the input file
+guard let reader = LineReader(path: "/Users/student/Desktop/github/MentalHealthWeek/survey_response_all_data_new_headers.csv") else {
     exit(0); // cannot open file
 }
 
-
-//
-// A function to parse the activity full name into the activity name.
-//
-func getActivityName (whole: String) -> String  {
-    var switchNow = 0
-    var charSetup = [Character]()
-    var output = ""
-    for char in whole.characters {
-        if switchNow == 2 {
-            charSetup.append(char)
-            
-        }
-        
-        if char == "_" {
-            switchNow += 1
-        }
-    }
-    output = String(charSetup)
-    return output
-}
-
-
-//
-// A function to parse the activity full name into the activitiy week day.
-//
-func getActivityDay (whole: String) -> String  {
-    var charSetup = [Character]()
-    var output = ""
-    for char in whole.characters {
-        if char == "_" {
-            output = String(charSetup)
-            return output
-        }
-        charSetup.append(char)
-    }
-    return output
-}
-
-
-for (number, line) in reader.enumerated()
+for (number, line) in reader.enumerated() // Go through the data and process each student
 {
-    if number == 0
+    if number == 0 // If we are processing the titles
     {
         columnDescriptors = line.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).components(separatedBy: ",")
         
         for (column, descriptor) in columnDescriptors.enumerated()
         {
-            descriptorLookup.append(descriptor) // Build descriptor lookup table
+            titles.append(descriptor) // Build descriptor lookup table
         }
-    } else {
+    } else { // If we are processing a student
         columnDescriptors = line.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).components(separatedBy: ",")
         
-        var studentEmail = columnDescriptors[5]
-        var studentGrade = columnDescriptors[9]
-        var studentAdvisor = columnDescriptors[10]
-        var studentActivities : [String] = []
-        
-        
-        if let studentGradeInt = Int(studentGrade)
-        {
-            let index = studentGradeInt - 9
-            let gradeChoiceOffset = gradeChoiceNum[index] // An offset to get to the correct selection of survey choices
-            var offset = gradeChoiceLookup[studentGradeInt]! as Int
-            
-            var previousDayCharacter : Character = "M"
-            var activityRankings : [String] = []
-            var dayNames : [String] = []
-            
-            for i in 0...gradeChoiceOffset - 1 // Iterate of all of the survey choices
-            {
-                let currentChoiceIndex = activityChoiceOffset + offset + i
-                //var currentChoice = columnDescriptors[currentChoiceIndex]
-                
-                let currentDay = descriptorLookup[currentChoiceIndex]
-                
-                let currentDayCharacter = currentDay[currentDay.startIndex]
-                
-                if (currentDayCharacter != previousDayCharacter || i == gradeChoiceOffset - 1)
-                {
-                    if (i == gradeChoiceOffset - 1)
-                    {
-                        if let activityRank = String(columnDescriptors[currentChoiceIndex])
-                        {
-                            activityRankings.append(activityRank)
-                            dayNames.append(currentDay)
-                        }
-                    }
-                    
-                    for ranking in 1...8
-                    {
-                        if let currentRankingIndex = activityRankings.index(of: String(ranking))
-                        {
-                            let currentFullActivityName = dayNames[currentRankingIndex]
-                            let currentActivityName = getActivityName(whole: currentFullActivityName)
-                            let currentActivityWeekday = getActivityDay(whole: currentFullActivityName)
-                            if let weekdayIndex = weekdayLookup[currentActivityWeekday]
-                            {
-                                if var currentActivity = activities.filter({$0.shortName == currentActivityName }).first
-                                {
-                                    if (currentActivity.weekdays[weekdayIndex].count < currentActivity.personCap)
-                                    {
-                                        if let activityIndex = activities.index(of: currentActivity)
-                                        {
-                                            activities[activityIndex].weekdays[weekdayIndex].append(studentEmail)
-                                            studentActivities.append(currentActivity.name)
-                                            
-                                            break
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        
-                    }
-                    
-                    dayNames = []
-                    activityRankings = []
-                }
-                
-                if let activityRank = String(columnDescriptors[currentChoiceIndex])
-                {
-                    activityRankings.append(activityRank)
-                    dayNames.append(currentDay)
-                }
-                
-                previousDayCharacter = currentDayCharacter
-            }
-            
-            students.append(Student(activities: studentActivities, email: studentEmail, advisor: studentAdvisor))
-        }
+        let student = input.getAndAssignStudent(data: columnDescriptors, titles: titles) // Get the student and assign them to the activity
+        students.append(student) // Append the student to a list of students
     }
 }
-//print(activities)
-//print(students)
+print(activities)
+print(students)
 
 //___oo#:#o__
 //o##########o###-
@@ -197,98 +76,98 @@ for (number, line) in reader.enumerated()
 //######:o-    ~#_ ~##o_      __-~~####oo_~~~~-_:~ ##o_   ~~o_o-~~
 //######~~~      ~o:~~###o--~~      ~~~####oo_  ~~  ~###  ##o~~-_
 
-
-func format (baseWord: String) -> String {
-    
-    let standardAmount = 20
-    var needChar = 0
-    var charAmount = 0
-    var newChar = [Character]()
-    
-    for char in baseWord.characters {
-        newChar.append(char)
-        charAmount += 1
-    }
-    if (standardAmount > charAmount) {
-        needChar = standardAmount - charAmount
-    }
-    for _ in 0...needChar {
-        newChar.append(" ")
-    }
-    let newString = String(newChar)
-    return newString
-}
-
-
-var week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-
-guard let writer = LineWriter(path: "/Users/student/Desktop/MentalHealthWeekFinal/survey_output.txt", appending: false) else {
-    print("Cannot open output file")
-    exit(0); // cannot open output file
-}
-
-func indivdualTime () {
-    //var activites : [String] =
-    for i in 0...students.count-3 {
-        //if
-        writer.write(line: "\(format(baseWord: students[i].email)) -> \(format(baseWord:  students[i].advisor))")
-        writer.write(line: "" )
-        
-        writer.write(line: "\(format(baseWord: week[0]))\(format(baseWord: week[1]))\(format(baseWord: week[2]))\(format(baseWord: week[3]))\(format(baseWord: week[4]))")
-        writer.write(line: "" )
-        
-        writer.write(line: "\(format(baseWord: students[i].activities[0]))\(format(baseWord: students[i].activities[1]))\(format(baseWord: students[i].activities[2]))\(format(baseWord: students[i].activities[3]))\(format(baseWord: students[i].activities[4]))")
-        writer.write(line: "" )
-        writer.write(line: "______________________________________________________________________________________________" )
-        writer.write(line: "" )
-    }
-}
-
-
-func superVisorList2 (){
-    for i in 1...activities.count-1 {
-        
-        writer.write(line: "")
-        writer.write(line: "______________________________" )
-        writer.write(line: "")
-        
-        print(activities[i].name)
-        print(activities[i].supervisorName)
-        print("")
-        
-        writer.write(line: "\(activities[i].name)")
-        writer.write(line: "\(activities[i].supervisorName)")
-        
-        for j in 0...4 {
-            print("")
-            print(week[j])
-            print("")
-            writer.write(line: "")
-            writer.write(line: "\(format(baseWord: week[j]))")
-            
-            if activities[i].weekdays[j].count-1 > 0 {
-                for l in 0...activities[i].weekdays[j].count-1{
-                    
-                    print(activities[i].weekdays[j][l])
-                    
-                    writer.write(line: "\(format(baseWord: activities[i].weekdays[j][l]))")
-
-                }
-            }
-        }
-    }
-
-}
-
-
-superVisorList2()
-//indivdualTime()
-
-// Close the output file
-writer.close() // MAKE NOTE OF THIS
-
-
-
-
-
-
+//
+//func format (baseWord: String) -> String {
+//    
+//    let standardAmount = 20
+//    var needChar = 0
+//    var charAmount = 0
+//    var newChar = [Character]()
+//    
+//    for char in baseWord.characters {
+//        newChar.append(char)
+//        charAmount += 1
+//    }
+//    if (standardAmount > charAmount) {
+//        needChar = standardAmount - charAmount
+//    }
+//    for _ in 0...needChar {
+//        newChar.append(" ")
+//    }
+//    let newString = String(newChar)
+//    return newString
+//}
+//
+//
+//var week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+//
+//guard let writer = LineWriter(path: "/Users/student/Desktop/MentalHealthWeekFinal/survey_output.txt", appending: false) else {
+//    print("Cannot open output file")
+//    exit(0); // cannot open output file
+//}
+//
+//func indivdualTime () {
+//    //var activites : [String] =
+//    for i in 0...students.count-3 {
+//        //if
+//        writer.write(line: "\(format(baseWord: students[i].email)) -> \(format(baseWord:  students[i].advisor))")
+//        writer.write(line: "" )
+//        
+//        writer.write(line: "\(format(baseWord: week[0]))\(format(baseWord: week[1]))\(format(baseWord: week[2]))\(format(baseWord: week[3]))\(format(baseWord: week[4]))")
+//        writer.write(line: "" )
+//        
+//        writer.write(line: "\(format(baseWord: students[i].activities[0]))\(format(baseWord: students[i].activities[1]))\(format(baseWord: students[i].activities[2]))\(format(baseWord: students[i].activities[3]))\(format(baseWord: students[i].activities[4]))")
+//        writer.write(line: "" )
+//        writer.write(line: "______________________________________________________________________________________________" )
+//        writer.write(line: "" )
+//    }
+//}
+//
+//
+//func superVisorList2 (){
+//    for i in 1...activities.count-1 {
+//        
+//        writer.write(line: "")
+//        writer.write(line: "______________________________" )
+//        writer.write(line: "")
+//        
+//        print(activities[i].name)
+//        print(activities[i].supervisorName)
+//        print("")
+//        
+//        writer.write(line: "\(activities[i].name)")
+//        writer.write(line: "\(activities[i].supervisorName)")
+//        
+//        for j in 0...4 {
+//            print("")
+//            print(week[j])
+//            print("")
+//            writer.write(line: "")
+//            writer.write(line: "\(format(baseWord: week[j]))")
+//            
+//            if activities[i].weekdays[j].count-1 > 0 {
+//                for l in 0...activities[i].weekdays[j].count-1{
+//                    
+//                    print(activities[i].weekdays[j][l])
+//                    
+//                    writer.write(line: "\(format(baseWord: activities[i].weekdays[j][l]))")
+//
+//                }
+//            }
+//        }
+//    }
+//
+//}
+//
+//
+//superVisorList2()
+////indivdualTime()
+//
+//// Close the output file
+//writer.close() // MAKE NOTE OF THIS
+//
+//
+//
+//
+//
+//
